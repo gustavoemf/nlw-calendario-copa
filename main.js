c0548632d52670ecd06a593c69479c98039021b3
gustavomullerf <gustavoeduardomf@gmail.com>
2022-11-03 21:23:10 -0300

    update dia 3-4 v2

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ function createGame(player1, hour, player2) {
     `;
 }
 
+delay = -0.4;
 function createCard(date, day, games) {
+  delay = delay + 0.4;
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
         <h2>${date} <span>${day}</span></h2>
         <ul>
             ${games}
@@ -20,9 +22,8 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-    createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
-    
-    createCard("28/11", "segunda", createGame("switzerland", "13:00", "brazil") + 
-    createGame("portugal", "16:00", "uruguay")) +
-    
-    createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+
+  createCard("28/11", "segunda", createGame("switzerland", "13:00", "brazil") + createGame("portugal", "16:00", "uruguay")) +
+
+  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"));
